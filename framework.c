9be552ac8dec48a4797354efe45a893b196e14b5
charlieoestreicher <charlie.oestreicher@gmail.com>
2024-04-01 11:38:36 -0400

    asking for password

--- framework.c
+++ framework.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main() {
-   puts("Hello, World!");
-   return 0;
+    char* password;
+    char correctPassword[] = "secret";
+    char location[] = "our classroom";
+
+    printf("Enter the password: ");
+    scanf("%s", password);
+
+    if (strcmp(password, correctPassword) == 0) {
+        printf("Password correct. Location revealed: %s\n", location);
+    } else {
+        printf("Incorrect password. Access denied.\n");
+    }
+
+    return 0;
 }
